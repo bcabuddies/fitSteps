2d487db50593022acc04ae0b7ee67980a1dbd21f
rsrsingh <34256496+rsrsingh@users.noreply.github.com>
2018-10-20 02:35:22 +0530

    firebase connected

--- app/src/main/java/com/bcabuddies/fitsteps/RegisterFirstFrag.java
+++ app/src/main/java/com/bcabuddies/fitsteps/RegisterFirstFrag.java
@@ -8,6 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 /**
@@ -16,8 +22,12 @@ import android.widget.Button;
 public class RegisterFirstFrag extends Fragment {
 
 
-    private TextInputEditText fullName, userEmail, userPass, userCpass;
+    private TextInputEditText userEmail, userPass, userCpass;
     private Button btnSubmit;
+    private FirebaseAuth auth;
+    private String email, password, confirmPass;
+    private FirebaseFirestore firebaseFirestore;
+    private String userId;
 
 
     public RegisterFirstFrag() {
@@ -30,13 +40,46 @@ public class RegisterFirstFrag extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register_first, container, false);
 
-        fullName = view.findViewById(R.id.regiter_nameEditText);
+
         userEmail = view.findViewById(R.id.regiter_emailEditText);
         userPass = view.findViewById(R.id.register_passEditText);
         userCpass = view.findViewById(R.id.register_CpassEditText);
         btnSubmit = view.findViewById(R.id.register_btnSubmit);
+        auth = FirebaseAuth.getInstance();
 
-        // Inflate the layout for this fragment
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = userEmail.getText().toString();
+                password = userPass.getText().toString();
+                confirmPass = userCpass.getText().toString();
+
+
+                if (email.equals("") || password.equals("")) {
+                    Toast.makeText(getContext(), "Please fill all info.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (password.equals(confirmPass)) {
+
+                        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+
+                                Toast.makeText(getContext(), "User created successfully", Toast.LENGTH_SHORT).show();
+                                RegisterSecondFrag registerSecondFrag = new RegisterSecondFrag();
+                                getFragmentManager().beginTransaction().replace(R.id.registerMain_frame, registerSecondFrag).commit();
+                            }
+                        });
+                    } else {
+                        Toast.makeText(getContext(), "Password did not match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
+
+
         return view;
     }
 
--- app/src/main/java/com/bcabuddies/fitsteps/Welcome.java
+++ app/src/main/java/com/bcabuddies/fitsteps/Welcome.java
@@ -1,56 +1,92 @@
 package com.bcabuddies.fitsteps;
 
 import android.content.Intent;
-import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Welcome extends AppCompatActivity {
 
 
     private static final String TAG = "Welcome.java";
     private Window mWindow;
-
-    private TextInputEditText userEmail,userPass;
+    private FirebaseAuth auth;
+    private String email, password;
+    private TextInputEditText userEmail, userPass;
     private Button btnLogin;
     TextView txtReg, txtForgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.WelcomeAppTheme);
         mWindow = getWindow();
         mWindow.getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
-        userEmail=findViewById(R.id.login_emailEditText);
-        userPass=findViewById(R.id.login_passwordEditText);
-        btnLogin=findViewById(R.id.login_loginBtn);
-        txtReg=findViewById(R.id.login_register);
-        txtForgotPass=findViewById(R.id.login_forgetPassword);
+        auth = FirebaseAuth.getInstance();
+        userEmail = findViewById(R.id.login_emailEditText);
+        userPass = findViewById(R.id.login_passwordEditText);
+        btnLogin = findViewById(R.id.login_loginBtn);
+        txtReg = findViewById(R.id.login_register);
+        txtForgotPass = findViewById(R.id.login_forgetPassword);
+
 
         txtReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Welcome.this,RegisterMain.class));
+                startActivity(new Intent(Welcome.this, RegisterMain.class));
             }
         });
 
         txtForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Welcome.this,ForgotPass.class));
+                startActivity(new Intent(Welcome.this, ForgotPass.class));
             }
         });
 
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = userEmail.getText().toString();
+                password = userPass.getText().toString();
+                if (!(email.equals("") || password.equals(""))) {
+                    auth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(Welcome.this, "Sucessfully Logged in", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Welcome.this,Home.class));
+                            finish();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Welcome.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    Toast.makeText(Welcome.this, "Enter email and password", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
 
 
     }
