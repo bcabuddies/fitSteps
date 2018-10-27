c1e42bd7e81e8731adfccb76bcf8657189f85d74
Rahul Gaur <rahul.gaur152@gmail.com>
2018-10-27 17:40:48 +0530

    RegisterSecondFrag: Name is uploaded in "Users/userID/" need to do: photo picker, photo compressor, photo upload to firebase and link updated to Users database

--- app/src/main/java/com/bcabuddies/fitsteps/RegisterSecondFrag.java
+++ app/src/main/java/com/bcabuddies/fitsteps/RegisterSecondFrag.java
@@ -1,7 +1,9 @@
 package com.bcabuddies.fitsteps;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,9 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,9 +31,10 @@ public class RegisterSecondFrag extends Fragment {
     private TextInputEditText fullName;
     private Button btnNext;
     private CircleImageView thumbImage;
-    String name,userId;
-    FirebaseFirestore firebaseFirestore;
-    FirebaseAuth auth;
+    private String name, userId;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth auth;
+    private Context context;
 
     public RegisterSecondFrag() {
         // Required empty public constructor
@@ -41,15 +49,38 @@ public class RegisterSecondFrag extends Fragment {
         btnNext = view.findViewById(R.id.register2_btnNext);
         thumbImage = view.findViewById(R.id.register2_profile);
         fullName = view.findViewById(R.id.register2_fullName);
-        auth=FirebaseAuth.getInstance();
-        userId=auth.getCurrentUser().getUid();
-        Log.e("userid", "onCreateView: "+ userId);
-        firebaseFirestore=FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        userId = auth.getCurrentUser().getUid();
+        context = getContext();
+        Log.e("userid", "onCreateView: " + userId);
+        firebaseFirestore = FirebaseFirestore.getInstance();
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RegisterThirdFrag registerThirdFrag=new RegisterThirdFrag();
-                getFragmentManager().beginTransaction().replace(R.id.registerMain_frame,registerThirdFrag).commit();
+
+                name = fullName.getText().toString();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(context, "Please Enter your name ", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    HashMap<String, Object> map = new HashMap<>();
+                    map.put("name", name);
+
+                    firebaseFirestore.collection("Users").document(userId).set(map)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        RegisterThirdFrag registerThirdFrag = new RegisterThirdFrag();
+                                        getFragmentManager().beginTransaction().replace(R.id.registerMain_frame, registerThirdFrag).commit();
+                                    } else {
+                                        Toast.makeText(context, "some error " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+                }
             }
         });
 
