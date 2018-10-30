e7ca641eb8428e726d5cd56fca00b30361e402be
rsrsingh <34256496+rsrsingh@users.noreply.github.com>
2018-10-30 16:01:58 +0530

    home side navigation design

--- app/src/main/java/com/bcabuddies/fitsteps/Home.java
+++ app/src/main/java/com/bcabuddies/fitsteps/Home.java
@@ -1,14 +1,19 @@
 package com.bcabuddies.fitsteps;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
 
     private Button runBtn;
+    private ImageView toogleNavigation;
+    private DrawerLayout sideDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,16 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         runBtn = findViewById(R.id.home_toolbar_runBtn);
+        sideDrawerLayout=findViewById(R.id.home_drawerLayout);
+        toogleNavigation=findViewById(R.id.home_toolbar_menuBtn);
+
+        toogleNavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sideDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
 
         runBtn.setOnClickListener(new View.OnClickListener() {
             @Override
